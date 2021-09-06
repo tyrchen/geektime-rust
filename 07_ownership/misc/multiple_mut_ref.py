a1c9ea5385c1a18da4dcc394bb9dd2893e6de39c
Tyr Chen <tyr.chen@gmail.com>
2021-09-05 23:06:06 -0700

    update 07. Remove code belongs to 08

--- 07_ownership/misc/multiple_mut_ref.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    data = [1, 2]
-    for item in data:
-        data.append(item + 1)
-        print(item)
-    print(data)
--- 07_ownership/src/outlive_heap.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let mut data: Vec<&u32> = Vec::new();
-    let v = 42;
-    data.push(&v);
-    println!("data: {:?}", data);
-}
